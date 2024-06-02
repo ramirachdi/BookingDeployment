f95bd5cf56700fd2a3d8b4ba3af5249d52edfdd9
ramirachdi <rachdirami11@gmail.com>
2024-06-02 11:45:59 +0100

    testing backend pipeline

--- BookingBackend/src/users/dto/create-user.dto.ts
+++ BookingBackend/src/users/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsOptional } from "class-validator";
 import { UserRoleEnum } from "../../enums/user-role.enum";
-
+//test
 export class CreateUserDto {
     @IsNotEmpty()
     name: string;
